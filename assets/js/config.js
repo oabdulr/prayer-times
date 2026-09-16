586946fba3b95086d707a4e8c786e2b3dcfe3ec4
Osamah <144569041+oabdulr@users.noreply.github.com>
2026-09-16 19:59:49 -0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ const PRAYER_CONFIG = Object.freeze({
   coordinates: [35.227, -80.843],
   timeZone: 'America/New_York',
   method: 'Makkah',
-  iqamah: { fajr: '06:00', dhuhr: '13:30', asr: '17:15', isha: '21:15' },
+  iqamah: { fajr: '06:15', dhuhr: '13:30', asr: '17:00', isha: '21:00' },
   maghribOffset: 5,
   updateInterval: 60_000,
 });
